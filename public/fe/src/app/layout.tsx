d54cc4f2a10a1c52315ead3dd072d9d65b96e8de
Nator Verinumbe <natverior1@gmail.com>
2024-02-03 19:39:14 +0100

    basic login screen

--- public/fe/src/app/layout.tsx
+++ public/fe/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
 
-
 // const inter = Inter({ subsets: ["latin"] });
 
 import { cn } from "../lib/utils";
