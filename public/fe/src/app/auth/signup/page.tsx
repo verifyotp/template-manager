0a14a8c702b7cc465b76e1319d30846b0d74bf1e
Nator Verinumbe <natverior1@gmail.com>
2024-02-18 12:38:21 +0100

    ...

---
+++ public/fe/src/app/auth/signup/page.tsx
@@ -0,0 +1,59 @@
+import { Metadata } from "next"
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/registry/new-york/ui/button"
+import { UserSignupForm } from "@/components/authentication/signup-form"
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+  description: "Sign up for an account",
+}
+
+export default function SignUpPage() {
+  return (
+    <>
+      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-1 lg:px-0">
+        <Link
+          href="/auth/login"
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            "absolute right-4 top-4 md:right-8 md:top-8"
+          )}
+        >
+          Login
+        </Link>
+        <div className="lg:p-auto">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-8 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Create an account
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email below to create your account
+              </p>
+            </div>
+            <UserSignupForm />
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{" "}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{" "}
+              and{" "}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
