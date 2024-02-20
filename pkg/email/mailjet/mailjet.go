bd4eac546b276bc347dfba630c74b9c22486093b
qalifah <oqalifah@gmail.com>
2024-02-20 07:50:15 +0100

    feat: complete mailjet integration

--- pkg/email/mailjet/mailjet.go
+++ pkg/email/mailjet/mailjet.go
@@ -1,17 +1,11 @@
 package mailjet
 
 import (
-	// "context"
-	// "errors"
-	// "fmt"
-	// "log"
-
 	jsoniter "github.com/json-iterator/go"
 	"template-manager/pkg/email"
 
 	mailjet "github.com/mailjet/mailjet-apiv3-go/v4"
 	"github.com/mailjet/mailjet-apiv3-go/v4/resources"
-	// "template-manager/internal/pkg/email"
 )
 
 type Mailjet struct {
@@ -24,21 +18,28 @@ func New() *Mailjet {
 	}
 }
 
-func (m *Mailjet) validateCredential(credential *email.TemplateInput) error {
-	if credential.PublicKey == "" {
+func (m *Mailjet) initMailjetClient(input *email.AuthCredential) (*mailjet.Client, error) {
+	if err := m.validateCredential(input); err != nil {
+		return nil, err
+	}
+	return mailjet.NewMailjetClient(input.PublicKey, input.PrivateKey), nil
+}
+
+func (m *Mailjet) validateCredential(input *email.AuthCredential) error {
+	if input.PublicKey == "" {
 		return email.ErrPublicKeyRequired
 	}
-	if credential.PrivateKey == "" {
+	if input.PrivateKey == "" {
 		return email.ErrPrivateKeyRequired
 	}
 	return nil
 }
 
-func (m *Mailjet) GetTemplates(credential *email.TemplateInput) (*email.TemplateList, error) {
-	if err := m.validateCredential(credential); err != nil {
+func (m *Mailjet) GetTemplates(input *email.TemplateQuery) (*email.TemplateList, error) {
+	client, err := m.initMailjetClient(&input.AuthCredential)
+	if err != nil {
 		return nil, err
 	}
-	client := mailjet.NewMailjetClient(credential.PublicKey, credential.PrivateKey)
 	var data []resources.Template
 	count, total, err := client.List("template", &data)
 	if err != nil {
@@ -58,15 +59,15 @@ func (m *Mailjet) GetTemplates(credential *email.TemplateInput) (*email.Template
 	return response, nil
 }
 
-func (m *Mailjet) GetTemplate(credential *email.TemplateInput) (*email.TemplateList, error) {
-	if err := m.validateCredential(credential); err != nil {
+func (m *Mailjet) GetTemplate(input *email.TemplateQuery) (*email.TemplateList, error) {
+	client, err := m.initMailjetClient(&input.AuthCredential)
+	if err != nil {
 		return nil, err
 	}
-	client := mailjet.NewMailjetClient(credential.PublicKey, credential.PrivateKey)
 	var data []resources.Template
 	mr := &mailjet.Request{
 		Resource: "template",
-		ID:       credential.ID,
+		ID:       input.ID,
 	}
 	if err := client.Get(mr, &data); err != nil {
 		return nil, err
@@ -75,22 +76,101 @@ func (m *Mailjet) GetTemplate(credential *email.TemplateInput) (*email.TemplateL
 	if err != nil {
 		return nil, err
 	}
-	response := &email.TemplateList{}
+	response := new(email.TemplateList)
 	if err := m.marshaler.Unmarshal(marshaledData, response.Data); err != nil {
 		return nil, err
 	}
 	return response, nil
 }
 
-func (m *Mailjet) GetTemplateContent(credential *email.TemplateInput) (*email.TemplateContent, error) {
-	if err := m.validateCredential(credential); err != nil {
+func (m *Mailjet) AddTemplate(input *email.TemplateInput) (*email.TemplateList, error) {
+	client, err := m.initMailjetClient(&input.AuthCredential)
+	if err != nil {
 		return nil, err
 	}
-	client := mailjet.NewMailjetClient(credential.PublicKey, credential.PrivateKey)
+	var data []resources.Template
+	mr := &mailjet.Request{
+		Resource: "template",
+	}
+	fmr := &mailjet.FullRequest{
+		Info: mr,
+		Payload: resources.Template{
+			Name:        input.Name,
+			Description: input.Description,
+			Author:      input.Author,
+		},
+	}
+	if err := client.Post(fmr, &data); err != nil {
+		return nil, err
+	}
+	marshaledData, err := m.marshaler.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+	response := new(email.TemplateList)
+	if err := m.marshaler.Unmarshal(marshaledData, response.Data); err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+func (m *Mailjet) UpdateTemplate(input *email.TemplateInput) error {
+	client, err := m.initMailjetClient(&input.AuthCredential)
+	if err != nil {
+		return err
+	}
+	fields := make([]string, 0)
+	payload := resources.Template{}
+	if input.Name != "" {
+		payload.Name = input.Name
+		fields = append(fields, "Name")
+	}
+	if input.Description != "" {
+		payload.Description = input.Description
+		fields = append(fields, "Description")
+	}
+	if input.Author != "" {
+		payload.Author = input.Author
+		fields = append(fields, "Author")
+	}
+	mr := &mailjet.Request{
+		Resource: "template",
+		ID:       input.ID,
+	}
+	fmr := &mailjet.FullRequest{
+		Info:    mr,
+		Payload: payload,
+	}
+	if err := client.Put(fmr, fields); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *Mailjet) DeleteTemplate(input *email.TemplateQuery) error {
+	client, err := m.initMailjetClient(&input.AuthCredential)
+	if err != nil {
+		return err
+	}
+	mr := &mailjet.Request{
+		Resource: "template",
+		ID:       input.ID,
+	}
+	if err := client.Delete(mr); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *Mailjet) GetTemplateContent(input *email.TemplateQuery) (*email.TemplateContentList, error) {
+	client, err := m.initMailjetClient(&input.AuthCredential)
+	if err != nil {
+		return nil, err
+	}
 	var data []resources.TemplateDetailcontent
 	mr := &mailjet.Request{
 		Resource: "template",
-		ID:       credential.ID,
+		ID:       input.ID,
 		Action:   "detailcontent",
 	}
 	if err := client.Get(mr, &data); err != nil {
@@ -100,9 +180,76 @@ func (m *Mailjet) GetTemplateContent(credential *email.TemplateInput) (*email.Te
 	if err != nil {
 		return nil, err
 	}
-	response := &email.TemplateContent{}
+	response := new(email.TemplateContentList)
 	if err := m.marshaler.Unmarshal(marshaledData, response); err != nil {
 		return nil, err
 	}
 	return response, nil
+}
+
+func (m *Mailjet) AddTemplateContent(input *email.TemplateInput) (*email.TemplateContentList, error) {
+	client, err := m.initMailjetClient(&input.AuthCredential)
+	if err != nil {
+		return nil, err
+	}
+	var data []resources.TemplateDetailcontent
+	mr := &mailjet.Request{
+		Resource: "template",
+		ID:       input.ID,
+		Action:   "detailcontent",
+	}
+	fmr := &mailjet.FullRequest{
+		Info: mr,
+		Payload: resources.TemplateDetailcontent{
+			Headers:  input.Headers,
+			HtmlPart: input.HTMLContent,
+			TextPart: input.TextContent,
+		},
+	}
+	if err := client.Post(fmr, &data); err != nil {
+		return nil, err
+	}
+	marshaledData, err := m.marshaler.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+	response := new(email.TemplateContentList)
+	if err := m.marshaler.Unmarshal(marshaledData, response); err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+func (m *Mailjet) UpdateTemplateContent(input *email.TemplateInput) error {
+	client, err := m.initMailjetClient(&input.AuthCredential)
+	if err != nil {
+		return err
+	}
+	fields := make([]string, 0)
+	payload := resources.TemplateDetailcontent{}
+	if input.HTMLContent != "" {
+		payload.HtmlPart = input.HTMLContent
+		fields = append(fields, "HtmlPart")
+	}
+	if input.TextContent != "" {
+		payload.TextPart = input.TextContent
+		fields = append(fields, "TextPart")
+	}
+	if input.Headers != nil {
+		payload.Headers = input.Headers
+		fields = append(fields, "Headers")
+	}
+	mr := &mailjet.Request{
+		Resource: "template",
+		ID:       input.ID,
+		Action:   "detailcontent",
+	}
+	fmr := &mailjet.FullRequest{
+		Info:    mr,
+		Payload: payload,
+	}
+	if err := client.Put(fmr, fields); err != nil {
+		return err
+	}
+	return nil
 }
