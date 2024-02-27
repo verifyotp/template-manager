f6cde968254495dcc67ed922305644b62657d904
Nator Verinumbe <natverior1@gmail.com>
2024-02-27 22:46:19 +0100

    ...mobile response

--- public/fe/src/components/auth/reset-form.tsx
+++ public/fe/src/components/auth/reset-form.tsx
@@ -70,7 +70,7 @@ export function ResetForm({ className, ...props }: UserAuthFormProps) {
 
   return (
     <div className="container relative h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-1 lg:px-0">
-      <div className="lg:p-auto">
+      <div className="lg:p-auto  pt-10 ">
         <div className="mx-auto flex w-full flex-col justify-center space-y-8 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
--- public/fe/src/components/auth/signup-form.tsx
+++ public/fe/src/components/auth/signup-form.tsx
@@ -8,8 +8,8 @@ import { Icons } from "@/components/ui/icons"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/new-york/input"
 import { signUpRequest as signUp } from "@/actions/signup"
-import { useRouter } from 'next/navigation'
-import { useToast } from "@/components/ui/use-toast"
+import Link from "next/link"
+import { buttonVariants } from "@/components/ui/button"
 import {
   Form,
   FormControl,
@@ -63,43 +63,82 @@ export function SignupForm({ className, ...props }: SignupFormProps) {
   };
 
   return (
-    <div className={cn("grid gap-9", className)} {...props}>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-        >
-          <div className="grid gap-5">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                      placeholder="name@example.com"
-                      type="email"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormError message={error} />
-            <FormSuccess message={success} />
-            <Button disabled={isPending}>
-              {isPending && (
-                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-              )}
-              Sign Up
-            </Button>
+    <div className="container relative h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-1 lg:px-0">
+
+      <div className="lg:p-auto pt-10 ">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-8 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Create an account
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your email below to create your account
+            </p>
           </div>
-        </form>
-      </Form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
+          <div className={cn("grid gap-9", className)} {...props}>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+              >
+                <div className="grid gap-5">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            disabled={isPending}
+                            placeholder="name@example.com"
+                            type="email"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormError message={error} />
+                  <FormSuccess message={success} />
+                  <Button disabled={isPending}>
+                    {isPending && (
+                      <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                    )}
+                    Sign Up
+                  </Button>
+                </div>
+              </form>
+            </Form>
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+            </div>
+            <div className="flex items-center justify-center">
+              <Button variant="link" >
+                <a href="/auth/login" className="font-small p-2 text-primary opacity-[0.60] hover:opacity-[1.0]">
+                  Have an account? Login
+                </a>
+              </Button>
+            </div>
+          </div>
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            By clicking sign up, you agree to our{" "}
+            <Link
+              href="/terms"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link
+              href="/privacy"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
         </div>
       </div>
     </div>
