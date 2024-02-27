13702aef82fc4f524e38b42aa28389b50d52013c
Nator Verinumbe <natverior1@gmail.com>
2024-02-27 21:16:12 +0100

    login page

--- public/fe/src/app/auth/login/page.tsx
+++ public/fe/src/app/auth/login/page.tsx
@@ -13,30 +13,7 @@ export const metadata: Metadata = {
 export default function LoginPage() {
   return (
     <>
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-1 lg:px-0">
-        {/* <Link
-          href="/auth/signup"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          Sign Up
-        </Link> */}
-        <div className="lg:p-auto">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-8 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-               Login
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
-              </p>
-            </div>
-            <LoginForm />
-          </div>
-        </div>
-      </div>
+      <LoginForm />
     </>
   )
 }
--- public/fe/src/components/auth/login-form.tsx
+++ public/fe/src/components/auth/login-form.tsx
@@ -53,12 +53,10 @@ export function LoginForm({ className, ...props }: LoginFormProps) {
       login(values)
         .then((data) => {
           if (data?.status === false) {
-            form.reset();
             setError(data.message);
           }
 
           if (data?.status) {
-            form.reset();
             setSuccess(data.message);
             localStorage.setItem('authToken', data.data?.session.token as string);
             // wait for the token to be set
@@ -72,86 +70,105 @@ export function LoginForm({ className, ...props }: LoginFormProps) {
   };
 
   return (
-    <div className={cn("grid gap-5", className)} {...props}>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-        >
-          <div className="grid gap-3">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                      placeholder="name@example.com"
-                      type="email"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="grid gap-1">
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        disabled={isPending}
-                        placeholder="******"
-                        type="password"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-            </div>
-            <FormError message={error} />
-            <FormSuccess message={success} />
-            <Button type="submit" disabled={isPending} >
-              {isPending && (
-                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-              )}
+    <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-1 lg:px-0">
+      <div className="lg:p-auto">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-8 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
               Login
-            </Button>
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your email and password to login
+            </p>
+          </div>
+          <div className={cn("grid gap-5", className)} {...props}>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+              >
+                <div className="grid gap-3">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            disabled={isPending}
+                            placeholder="name@example.com"
+                            type="email"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <div className="grid gap-1">
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <Input
+                              {...field}
+                              disabled={isPending}
+                              placeholder="******"
+                              type="password"
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
 
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 rounded border-gray-300 text-primary focus:ring-primary"
-                />
-                <Label htmlFor="remember-me" className="ml-2 text-sm">
-                  Remember me
-                </Label>
-              </div>
+                  </div>
+                  <FormError message={error} />
+                  <FormSuccess message={success} />
+                  <Button type="submit" disabled={isPending} >
+                    {isPending && (
+                      <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                    )}
+                    Login
+                  </Button>
 
-              <div className="text-sm">
-                <Button variant="link" >
-                  <a href="/auth/reset" className="font-medium text-primary">
-                    Forgot your password ?
-                  </a>
-                </Button>
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                      <input
+                        id="remember-me"
+                        type="checkbox"
+                        className="h-4 w-4 rounded border-gray-300 text-primary focus:ring-primary"
+                      />
+                      <Label htmlFor="remember-me" className="ml-2 text-sm">
+                        Remember me
+                      </Label>
+                    </div>
+
+                    <div className="text-sm">
+                      <Button variant="link" >
+                        <a href="/auth/reset" className="font-medium text-primary opacity-[0.70] hover:opacity-[1.0]">
+                          Forgot your password?
+                        </a>
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </Form>
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
               </div>
             </div>
+            <div className="flex items-center justify-center">
+              <Button variant="link" >
+                <a href="/auth/signup" className="font-small p-2 text-primary opacity-[0.60] hover:opacity-[1.0]">
+                  Don't have an account? Sign Up
+                </a>
+              </Button>
           </div>
-        </form>
-      </Form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
+          </div>
         </div>
       </div>
     </div>
